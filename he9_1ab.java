eb7672a40080dc99a732850b7ff8490ba26b655c
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-10-26 13:44:37 +0800

    Create he9_1ab.java

---
+++ he9_1ab.java
@@ -0,0 +1,34 @@
+package test;
+
+class CRectangle {
+	int width;
+	int height;
+
+	public CRectangle(int w, int h) {
+		width = w;
+		height = h;
+		System.out.println("constructor CRectangle(int w,int h) called");
+	}
+
+	public CRectangle() {
+		width = 10;
+		height = 8;
+		System.out.println("constructor CRectangle() called");
+	}
+
+	public void show() {
+		System.out.println("width=" + width);
+		System.out.println("height=" + height);
+	}
+}
+
+public class hw9_1ab {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CRectangle rec1 = new CRectangle(5, 2);
+		rec1.show();
+		CRectangle rec2 = new CRectangle();
+		rec2.show();
+	}
+
+}
